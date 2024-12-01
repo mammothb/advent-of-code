123485a5d4be146ff0446c178360a86a5488262c
mmb L <7960706+mammothb@users.noreply.github.com>
2024-12-01 13:20:03 +0800

    add day 1 Python solution

---
+++ 2024/day1.py
@@ -0,0 +1,29 @@
+import collections
+from pathlib import Path
+
+
+def main():
+    with open(Path(__file__).resolve().parent / "day1.txt") as infile:
+        l_arr: list[int] = []
+        r_arr: list[int] = []
+        for line in infile.readlines():
+            l, r = list(map(int, line.rstrip().split()))
+            l_arr.append(l)
+            r_arr.append(r)
+
+        l_arr = sorted(l_arr)
+        r_arr = sorted(r_arr)
+        result = 0
+        for l, r in zip(l_arr, r_arr):
+            result += abs(l - r)
+        print(result)
+
+        counter = collections.Counter(r_arr)
+        result2 = 0
+        for l in l_arr:
+            result2 += l * counter[l]
+        print(result2)
+
+
+if __name__ == "__main__":
+    main()
